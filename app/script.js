fbb007735e97bc3c24cb998a01745ea119e2f5db
Jordan Fallon <thefetts@gmail.com>
2020-01-10 22:05:28 -0500

    Many colorful ants!

--- app/script.js
+++ app/script.js
@@ -1,5 +1,3 @@
-const $ = (selector) => document.querySelector(selector)
-
 const timesDo = function(number, func) {
   let count = 0
   while(count < number) {
@@ -10,7 +8,7 @@ const timesDo = function(number, func) {
 
 const width = 200
 const height = 100
-const $grid = $('#grid')
+const $grid = document.querySelector('#grid')
 
 timesDo(height, row => {
   const rowDiv = document.createElement('div')
@@ -34,22 +32,31 @@ const Headings = {
 }
 
 class Ant {
-  x = Math.floor(width / 2)
-  y = Math.floor(height / 2)
+  x
+  y
+  color
   heading = Headings.E
 
-  move() {
-    const el = $(`#cell-${(this.x)}-${this.y}`)
+  constructor({
+    x = Math.floor(width / 2),
+    y = Math.floor(height / 2),
+    color = 'black'
+  }) {
+    this.x = x
+    this.y = y
+    this.color = color
+  }
 
-    // Pick a direction to move
+  move() {
+    const el = document.querySelector(`#cell-${(this.x)}-${this.y}`)
     if(el.classList.contains('on')) {
       this.changePosition('left')
       this.heading = Headings[this.heading.left]
-      el.classList.remove('on')
+      el.classList.remove('on', 'black', 'red', 'green', 'blue')
     } else {
       this.changePosition('right')
       this.heading = Headings[this.heading.right]
-      el.classList.add('on')
+      el.classList.add('on', this.color)
     }
   }
 
@@ -66,10 +73,15 @@ class Ant {
   }
 }
 
-const ant = new Ant()
+const ant = new Ant({x: 110})
+const ant2 = new Ant({x: 90, color: 'green'})
+const ant3 = new Ant({y: 40, color: 'red'})
+const ant4 = new Ant({y: 60, color: 'blue'})
+const ants = [ant, ant2, ant3, ant4]
+
 const interval = setInterval(() => {
   try {
-    ant.move()
+    ants.forEach(ant => ant.move())
   } catch (e) {
     console.log(`Error: ${e}`)
     clearInterval(interval)
